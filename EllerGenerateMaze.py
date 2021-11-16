9d707e0983db80557819a9051f44dda19f79665b
QuietRainto4 <93848789+QuietRainto4@users.noreply.github.com>
2021-11-15 21:50:26 -0500

    Create EllerGenerateMaze.py

---
+++ EllerGenerateMaze.py
@@ -0,0 +1,21 @@
+# Eller's method
+# https://weblog.jamisbuck.org/2011/1/10/maze-generation-prim-s-algorithm
+
+allSets = dict()
+listAllSet = []
+
+
+# initialize each column in row to be in their own set
+# put set in dictionary?
+# def firstRow():
+
+
+# randomly join adjacent cells if they are not in the same set
+
+# check if they are in the same set
+
+# randomly join sets vertically
+
+# make the rest of the rows their own set
+
+# if reach the last row, join all remaining cells that does not share a set
