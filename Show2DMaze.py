df09edda0036ba12bc41023a3f4773377af3bdea
QuietRainto4 <93848789+QuietRainto4@users.noreply.github.com>
2021-11-20 18:03:22 -0500

    Update Show2DMaze.py

--- Show2DMaze.py
+++ Show2DMaze.py
@@ -1,53 +1,73 @@
 from cmu_112_graphics import*
 from MazeClass import*
 
-def appStarted(app):
-    app.testMaze = Maze(25, 25, 100)
-    app.margin = 50
-    app.colWidth = (app.width - 2 * app.margin) / (len(app.testMaze.board[0]))
-    app.rowWidth = (app.height - 2 * app.margin) / (len(app.testMaze.board))
-    app.testMaze.generateMaze()
-    app.board = app.testMaze.board
-    app.pCol = 1
-    app.pRow = 1
-
 # draws the maze
-def drawMaze(app, canvas):
-    for numRow in range(len(app.board)):
-        for numCol in range(len(app.board[0])):
-            if app.board[numRow][numCol] == 1:
+def twoD_drawMaze(app, canvas):
+    for numRow in range(len(app.board2D)):
+        for numCol in range(len(app.board2D[0])):
+            if app.board2D[numRow][numCol] == 1:
                 canvas.create_rectangle(
-                    app.margin + app.colWidth * numCol,
-                    app.margin + app.rowWidth * numRow,
-                    app.margin + app.colWidth * (numCol + 1),
-                    app.margin + app.colWidth * (numRow + 1),
+                    app.margin2D + app.colWidth2D * numCol,
+                    app.margin2D + app.rowWidth2D * numRow,
+                    app.margin2D + app.colWidth2D * (numCol + 1),
+                    app.margin2D + app.colWidth2D * (numRow + 1),
                     fill = "black")
 
-def redrawAll(app, canvas):
-    drawMaze(app, canvas)
-    drawPlayer(app, canvas)
+def twoD_redrawAll(app, canvas):
+    twoD_drawMaze(app, canvas)
+    twoD_drawPlayer(app, canvas)
+    if app.finish2D == True:
+        twoD_drawEnd(app, canvas)
 
-def keyPressed(app, event):
+def twoD_keyPressed(app, event):
     if event.key == "Right":
-        movePlayer(app, 0, 1)
+        twoD_movePlayer(app, 0, 1)
     elif event.key == "Left":
-        movePlayer(app, 0, -1)
+        twoD_movePlayer(app, 0, -1)
     elif event.key == "Up":
-        movePlayer(app, -1, 0)
+        twoD_movePlayer(app, -1, 0)
     elif event.key == "Down":
-        movePlayer(app, 1, 0)
+        twoD_movePlayer(app, 1, 0)
+    elif event.key == 'r':
+        twoD_reset(app)
 
-def movePlayer(app, drow, dcol):
-    if app.board[app.pRow + drow][app.pCol + dcol] == 0:
-        app.pCol += dcol
-        app.pRow += drow
+def twoD_reset(app):
+    app.testMaze2D = Maze(10, 10, 100)
+    app.colWidth2D = (app.width - 2 * app.margin2D) / (len(app.testMaze2D.board[0]))
+    app.rowWidth2D = (app.height - 2 * app.margin2D) / (len(app.testMaze2D.board))
+    app.testMaze2D.generateMaze()
+    app.board2D = app.testMaze2D.board
+    app.pCol2D = 1
+    app.pRow2D = 1
+    app.finish2D = False
 
-def drawPlayer(app, canvas):
-    canvas.create_oval(app.margin + app.colWidth * app.pCol,
-                    app.margin + app.rowWidth * app.pRow,
-                    app.margin + app.colWidth * (app.pCol + 1),
-                    app.margin + app.colWidth * (app.pRow + 1),
+def twoD_movePlayer(app, drow, dcol):
+    if app.board2D[app.pRow2D + drow][app.pCol2D + dcol] == 0:
+        app.pCol2D += dcol
+        app.pRow2D += drow
+
+def twoD_drawPlayer(app, canvas):
+    canvas.create_oval(app.margin2D + app.colWidth2D * app.pCol2D,
+                    app.margin2D + app.rowWidth2D * app.pRow2D,
+                    app.margin2D + app.colWidth2D * (app.pCol2D + 1),
+                    app.margin2D + app.colWidth2D * (app.pRow2D + 1),
                     fill = "red")
 
-runApp(width = 800, height = 800)
+def twoD_drawEnd(app, canvas):
+    canvas.create_rectangle(app.width * 3 / 11, app.height * 4 / 11,
+                app.width * 8 / 11, app.height * 7 / 11, fill = "white",
+                outline = "black", width = app.width / 100)
+    canvas.create_text(app.width * 5.5 / 11, app.height * 5 / 11,
+                text = "Congradulations", font = "Ariel 24 bold")
+    canvas.create_text(app.width * 5.5 / 11, app.height * 5.5 / 11,
+                text = "Press R to play again", font = "Ariel 16 bold")
 
+def twoD_reachedEnd(app):
+    if (app.pRow2D == (len(app.board2D) - 2) and
+        app.pCol2D == (len(app.board2D[0]) - 2)):
+        app.finish2D = True
+    else:
+        app.finish2D = False
+
+def twoD_timerFired(app):
+    twoD_reachedEnd(app)
