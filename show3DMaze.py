5c8d0d25338c6337aa64d67c6914dcfb2132dca5
QuietRainto4 <93848789+QuietRainto4@users.noreply.github.com>
2021-11-20 18:03:25 -0500

    Update show3DMaze.py

--- show3DMaze.py
+++ show3DMaze.py
@@ -33,36 +33,7 @@ def print3dList(L):
     for layer in L:
         print2dList(layer)
 
-def appStarted(app):
-    app.testMaze = threeDMaze(5, 5, 5, 100)
-    app.testMaze.generate3DMaze()
-    app.board = app.testMaze.board
-    # setting the finishing position
-    app.board[len(app.board)-2][len(app.board[0])-2][len(app.board[0][0])-2] = 'e'
-    # the board with player
-    app.boardP = copy.deepcopy(app.board)
-    print3dList(app.board)
-    app.pCol = 1
-    app.pRow = 1
-    app.pHeight = 1 
-    app.finish = False
-    # the corners of the maze
-    # I divided the window into 11 * 11 boxes
-    app.zx1 = app.width * 1 / 11
-    app.zx2 = app.width * 5 / 11
-    app.zy1 = app.height * 1 / 11
-    app.zy2 = app.width * 5 / 11
-    app.xx1 = app.width * 1 / 11
-    app.xx2 = app.width * 5 / 11
-    app.xy1 = app.height * 6 / 11
-    app.xy2 = app.width * 10 / 11
-    app.yx1 = app.width * 6 / 11
-    app.yx2 = app.width * 10 / 11
-    app.yy1 = app.height * 6 / 11
-    app.yy2 = app.width * 10 / 11
-
-
-def drawMaze(app, canvas, board, x1, y1, x2, y2):
+def threeD_drawMaze(app, canvas, board, x1, y1, x2, y2):
     width = x2 - x1
     height = y2 - y1
     colWidth = width / len(board)
@@ -98,83 +69,84 @@ def drawMaze(app, canvas, board, x1, y1, x2, y2):
                     y1 + colWidth * (numRow + 1),
                     fill = "pink")
 
-def zSection(app):
-    return app.boardP[app.pHeight]
+def threeD_zSection(app):
+    return app.boardP3D[app.pHeight3D]
 
 # return the cross section of the board
-def xSection(app):
+def threeD_xSection(app):
     newBoard = []
-    for layer in app.boardP:
-        newBoard.insert(0, layer[app.pRow])
+    for layer in app.boardP3D:
+        newBoard.insert(0, layer[app.pRow3D])
     return newBoard
 
 # returns the y cross section of the board from the player's position
-def ySection(app):
+def threeD_ySection(app):
     newBoard = []
-    for layer in app.boardP:
+    for layer in app.boardP3D:
         column = []
         for row in layer:
-            column.append(row[app.pCol])
+            column.append(row[app.pCol3D])
         newBoard.insert(0, column)
     return newBoard
 
 # does action based on the key that is pressed
-def keyPressed(app, event):
+def threeD_keyPressed(app, event):
     if event.key == "Right":
-        movePlayer(app, 0, 1, 0)
+        threeD_movePlayer(app, 0, 1, 0)
     elif event.key == "Left":
-        movePlayer(app, 0, -1, 0)
+        threeD_movePlayer(app, 0, -1, 0)
     elif event.key == "Up":
-        movePlayer(app, -1, 0, 0)
+        threeD_movePlayer(app, -1, 0, 0)
     elif event.key == "Down":
-        movePlayer(app, 1, 0, 0)
+        threeD_movePlayer(app, 1, 0, 0)
     elif event.key == "z":
-        movePlayer(app, 0, 0, 1)
+        threeD_movePlayer(app, 0, 0, 1)
     elif event.key == "x":
-        movePlayer(app, 0, 0, -1)
+        threeD_movePlayer(app, 0, 0, -1)
+    # for chaning modes
+    if event.key == "2":
+        app.mode = "twoD"
 
 # move the position of the player
-def movePlayer(app, drow, dcol, dheight):
-    currPos = app.board[app.pHeight + dheight][app.pRow + drow][app.pCol + dcol]
+def threeD_movePlayer(app, drow, dcol, dheight):
+    currPos = app.board3D[app.pHeight3D + dheight][app.pRow3D + drow][app.pCol3D + dcol]
     if currPos == 0 or currPos == 'e':
-        app.pCol += dcol
-        app.pRow += drow
-        app.pHeight += dheight
+        app.pCol3D += dcol
+        app.pRow3D += drow
+        app.pHeight3D += dheight
 
 # this function is called each run
-def timerFired(app):
-    reachedEnd(app)
-    app.boardP = copy.deepcopy(app.board)
+def threeD_timerFired(app):
+    threeD_reachedEnd(app)
+    app.boardP3D = copy.deepcopy(app.board3D)
     # print3dList(app.board)
-    app.boardP[app.pHeight][app.pRow][app.pCol] = "p"
+    app.boardP3D[app.pHeight3D][app.pRow3D][app.pCol3D] = "p"
 
-def redrawAll(app, canvas):
+def threeD_redrawAll(app, canvas):
     # set the coordinate to what the person is in as 'p'
-    zBoard = zSection(app)
-    yBoard = ySection(app)
-    xBoard = xSection(app)
+    zBoard = threeD_zSection(app)
+    yBoard = threeD_ySection(app)
+    xBoard = threeD_xSection(app)
     canvas.create_text((app.zx1 + app.zx2) / 2, app.height * 1/22,
-                        text = f'z = {app.pHeight}')
-    drawMaze(app, canvas, zBoard, app.zx1, app.zy1, 
+                        text = f'z = {app.pHeight3D}')
+    threeD_drawMaze(app, canvas, zBoard, app.zx1, app.zy1, 
                 app.zx2, app.zy2)
     canvas.create_text((app.yx1 + app.yx2) / 2, app.height * 11/22,
-                        text = f'y = {app.pRow}')
-    drawMaze(app, canvas, yBoard, app.yx1, app.yy1, 
+                        text = f'y = {app.pRow3D}')
+    threeD_drawMaze(app, canvas, yBoard, app.yx1, app.yy1, 
                 app.yx2, app.yy2)
     canvas.create_text((app.xx1 + app.xx2) / 2, app.height * 11/22,
-                        text = f'x = {app.pCol}')
-    drawMaze(app, canvas, xBoard, app.xx1, app.xy1, 
+                        text = f'x = {app.pCol3D}')
+    threeD_drawMaze(app, canvas, xBoard, app.xx1, app.xy1, 
                 app.xx2, app.xy2)
     # return the board to original after
 
 # checks if p in on the end by 
-def reachedEnd(app):
-    if (app.pHeight == (len(app.board) - 2) and
-        app.pRow == (len(app.board[0]) - 2) and
-        app.pCol == (len(app.board[0][0]) - 2)):
+def threeD_reachedEnd(app):
+    if (app.pHeight3D == (len(app.board3D) - 2) and
+        app.pRow3D == (len(app.board3D[0]) - 2) and
+        app.pCol3D == (len(app.board3D[0][0]) - 2)):
         app.finished = True
     else:
         app.finished = False
                 
-
-runApp(width = 400, height = 400)
