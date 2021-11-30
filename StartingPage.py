15aebe2f40e2ad43ce30ded5905778ea8e3e1e43
QuietRainto4 <93848789+QuietRainto4@users.noreply.github.com>
2021-11-30 16:08:30 -0500

    Update StartingPage.py

--- StartingPage.py
+++ StartingPage.py
@@ -6,20 +6,11 @@ from ButtonClass import*
 # include help funciton and instructions
 def start_redrawAll(app, canvas):
     start_drawMaze(app, canvas)
-    canvas.create_image(app.width / 2, app.height * 3/11, 
+    canvas.create_image(app.width / 2, app.height * 2/11, 
                         image=ImageTk.PhotoImage(app.title))
     # draws the three boxes for differnt games
     for buttons in app.startButtons:
         buttons.drawButton(app, canvas)
-    # canvas.create_rectangle(app.width * 3/11, app.height * 5/11,
-    #                         app.width * 8/11, app.height * 6/11,
-    #                         fill = "white", width = 5)
-    # canvas.create_rectangle(app.width * 3/11, app.height * 6.5/11,
-    #                         app.width * 8/11, app.height * 7.5/11,
-    #                         fill = "white", width = 5)
-    # canvas.create_rectangle(app.width * 3/11, app.height * 8/11,
-    #                         app.width * 8/11, app.height * 9/11,
-    #                         fill = "white", width = 5)
     
 def start_drawMaze(app, canvas):
     for numRow in range(len(app.boardBack)):
@@ -29,23 +20,6 @@ def start_drawMaze(app, canvas):
                 y1 = app.rowWidthBack * numRow
                 x2 = app.colWidthBack * (numCol + 1)
                 y2 = app.colWidthBack * (numRow + 1)
-
-                # rotateMatrix = numpy.array([[math.cos(app.theta), -math.sin(app.theta)],
-                #                              [math.sin(app.theta), math.cos(app.theta)]])
-                # firstPoint = numpy.array([[x1], [y1]])
-                # secondPoint = numpy.array([[x2], [y2]])
-                # newfPoint = numpy.matmul(rotateMatrix, firstPoint)
-                # newsPoint = numpy.matmul(rotateMatrix, secondPoint)
-                # newX1 = newfPoint[0, 0]
-                # newY1 = newfPoint[1, 0]
-                # newX2 = newsPoint[0, 0]
-                # newY2 = newsPoint[1, 0]
-
-                # r1, theta1 = cartasianToPolar(app, x1, y1)
-                # r2, theta2 = cartasianToPolar(app, x2, y2)
-                # x1, y1 = polarToCartasian(app, r1, theta1)
-                # x2, y2 = polarToCartasian(app, r2, theta2)
-
                 canvas.create_rectangle(
                     x1, y1, x2, y2,
                     fill = "light grey", outline = "light grey")
@@ -54,25 +28,6 @@ def start_mousePressed(app, event):
     for buttons in app.startButtons:
         if buttons.inRectangle(event.x, event.y):
             buttons.pressed = True
-
-def cartasianToPolar(app, x, y):
-    centerX = app.width / 2
-    centerY = app.height / 2
-    r = distance(centerX, centerY, x, y)
-    theta = math.atan((centerY - y)/(centerX - x))
-    if (centerX - x) < 0:
-        theta = theta + math.pi
-    return r, theta
-    
-def polarToCartasian(app, r, theta):
-    x = r * math.cos(theta + app.theta)
-    y = r * math.sin(theta + app.theta)
-    return (x, y)
-
-
-def distance(centerX, centerY, x, y):
-    return ((centerX - x) ** 2 + (centerY - y) ** 2) ** 0.5
-
 
 def start_timerFired(app):
     app.theta += math.pi / 1000
@@ -85,6 +40,9 @@ def start_timerFired(app):
     elif app.startButtons[2].pressed == True:
         app.mode = 'layer3D'
         app.startButtons[2].pressed = False
+    elif app.startButtons[3].pressed == True:
+        app.mode = 'help'
+        app.startButtons[3].pressed = False
 
 
 
