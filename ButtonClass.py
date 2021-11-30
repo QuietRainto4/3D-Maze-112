7fb22758360a73cb77346468453da943a5d6619e
QuietRainto4 <93848789+QuietRainto4@users.noreply.github.com>
2021-11-29 20:11:07 -0500

    Update ButtonClass.py

--- ButtonClass.py
+++ ButtonClass.py
@@ -1,5 +1,8 @@
 from cmu_112_graphics import*
 
+# https://www.vectorstock.com/royalty-free-vector/on-and-off-icon-editable-switch-button-sign-vector-28695671
+
+
 class MyButton(App):
     def __init__(self, x1, y1, x2, y2, text, textSize):
         self.corners = (x1, y1, x2, y2)
@@ -27,7 +30,6 @@ class MyButton(App):
         canvas.create_text(cx, cy, text = self.text, 
                             font = f"Ariel {self.textSize}")
     
-    
 class CommandBar(MyButton):
     def __init__(self, x1, y1, x2, y2, textSize):
         super().__init__(x1, y1, x2, y2, "", textSize)
@@ -42,9 +44,18 @@ class OnOffButton(MyButton):
     def __init__(self, x1, y1, x2, y2, state):
         super().__init__(self, x1, y1, x2, y2, "", 0)
         self.state = state
-
-    def drawButton(self, x1, y1, x2, y2):
-        pass
+        
+    def drawButton(self, app, canvas):
+        onButton = app.loadImage('On Button.png')
+        offButton = app.loadImage('Off Button.png')
+        cx = (self.x1 + self.x2) / 2
+        cy = (self.y1 + self.y2) / 2
+        if self.state == True:
+            canvas.create_image(cx, cy, 
+                        image=ImageTk.PhotoImage(onButton))
+        else:
+            canvas.create_image(cx, cy, 
+                        image=ImageTk.PhotoImage(offButton))
 
             
             
